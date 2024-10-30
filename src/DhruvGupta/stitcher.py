d9edd205c192d9b752bfc6b238061e7927b50bcc
dg_20104 <amitgupta@iign.ac.in>
2024-10-30 06:37:43 +0530

    trying new way of impainting

--- src/DhruvGupta/stitcher.py
+++ src/DhruvGupta/stitcher.py
@@ -184,8 +184,8 @@ class PanaromaStitcher():
     def format_image(self, image):
         black_mask = cv2.inRange(image, (0, 0, 0), (0, 0, 0))
         non_black_mask = cv2.inRange(image, (1, 1, 1), (255, 255, 255))
-        kernel = np.ones((10, 10), np.uint8)
-        dilated_non_black_mask = cv2.dilate(non_black_mask, kernel, iterations=2)
+        kernel = np.ones((15, 15), np.uint8)
+        dilated_non_black_mask = cv2.dilate(non_black_mask, kernel, iterations=5)
         inpaint_mask = cv2.bitwise_and(black_mask, dilated_non_black_mask)
         inpainted_image = cv2.inpaint(image, inpaint_mask, inpaintRadius=3, flags=cv2.INPAINT_TELEA)
         return inpainted_image
